f4868c9aef17c2cb52250d246fc1cd063264b8a2
Erhan <erhantezer68@gmail.com>
2022-07-21 04:12:46 +0300

    m.card1

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,50 +1,9 @@
-import React, { useContext } from "react";
-import { useNavigate } from "react-router-dom";
-import { AuthContext } from "../context/AuthContext";
-import { toastWarnNotify } from "../helpers/ToastNotify";
+import React from 'react'
 
-const IMG_API = "https://image.tmdb.org/t/p/w1280";
-const defaultImage =
-  "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
-
-  //? alınacak verileri apı ye bakrak seçtik ve olduğu yerde yakaladık ekxtra props yapmadık çünkü verileri mainde spread etmiştik
-const MovieCard = ({ title, poster_path, overview, vote_average, id }) => {
-  const { currentUser } = useContext(AuthContext);
-  let navigate = useNavigate();
- 
-//? Detay için cardların bulunduğu div e onclick verdik eğer login olunmuşsa MovieDetail sayfasına navigate hook ile gönderdik tabiki id ile birlikte olmamışsa toastnotify yazdık
-//?  poster_path true ise img_api +poster_path eklenir ve basılır false ise default resim gelir
-//? Eğer kullanıcı login olmuşsa vote_avarage(oylama) ya göre renklendirsin olmamışsa direkt yazsın
+const MovieCard = () => {
   return (
-    <div
-      className="movie"
-      onClick={() =>currentUser ? navigate("details/" + id) : toastWarnNotify("Please log in to see details")
-        // : alert("Please log in to see details")
-      }
-    >
-      <img src={poster_path ? IMG_API + poster_path : defaultImage} alt="" />
+    <div>MovieCard</div>
+  )
+}
 
-      <div className="d-flex align-items-baseline justify-content-between p-1 text-white">
-
-        <h5>{title}</h5>
-
-        {currentUser && (
-          <span
-            className={`tag ${
-              vote_average >= 8 ? "green" : vote_average >= 6 ? "orange" : "red"
-            }`}
-          >
-            {vote_average}
-          </span>
-        )}
-      </div>
-      
-      <div className="movie-over">
-        <h2>Overview</h2>
-        <p>{overview}</p>
-      </div>
-    </div>
-  );
-};
-
-export default MovieCard;
+export default MovieCard
